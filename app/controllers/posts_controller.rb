138ffcea1ce6c2444ef16860430fd9c5b7673dfd
tk-16 <76659162+tk-16@users.noreply.github.com>
2021-01-07 23:54:08 +0900

    メモ投稿機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,18 +7,18 @@ class PostsController < ApplicationController
     post = Post.create(content: params[:content], checked: false)
     render json:{ post: post }
   end
-  end
-
-def checked
   
-  post = Post.find(params[:id])
-  if post.checked 
-    post.update(checked: false)
-  else
-    post.update(checked: true)
-  end
 
-  item = Post.find(params[:id])
-  render json: { post: item }
-end
+  def checked
+    
+    post = Post.find(params[:id])
+    if post.checked 
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
 end
